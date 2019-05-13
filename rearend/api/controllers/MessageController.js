3da0f089f57560831730a9713bab3a08a842950b
maoyining <2044964754@qq.com>
2019-05-13 20:04:21 +0800

    代码整齐度修改

--- rearend/api/controllers/MessageController.js
+++ rearend/api/controllers/MessageController.js
@@ -8,19 +8,7 @@
 module.exports = {
   
     hello: async function (req, res) {
-
-        // Make sure this is a socket request (not traditional HTTP)
-      //  if (!req.isSocket) {
-        //  return res.badRequest();
-        //}
-    
-        // var msg = req.param('msg');
-        // console.log(msg)
         sails.sockets.join(req, 'funSockets');
-        
-      //  let info=await Book.find({id:2}).populate('user')
-      //  let num =info[0].user.length
-        
         sails.sockets.broadcast('funSockets', { greeting: "欢迎加入funSockets房间" });
       },
 }; 
