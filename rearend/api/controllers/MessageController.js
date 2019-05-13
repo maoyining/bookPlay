2c69f47f825f94245ee7e20b43b87af4723e1581
maoyining <2044964754@qq.com>
2019-05-13 18:42:23 +0800

    websocket功能完善

---
+++ rearend/api/controllers/MessageController.js
@@ -0,0 +1,27 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+    hello: async function (req, res) {
+
+        // Make sure this is a socket request (not traditional HTTP)
+      //  if (!req.isSocket) {
+        //  return res.badRequest();
+        //}
+    
+        // var msg = req.param('msg');
+        // console.log(msg)
+        sails.sockets.join(req, 'funSockets');
+        
+        let info=await Book.find({id:2}).populate('user')
+        let num =info[0].user.length
+        
+        sails.sockets.broadcast('funSockets', { greeting: num });
+      },
+}; 
+
