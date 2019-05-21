fff4f4cfc41021be7293e1666420438f5baeb213
hanshirong <1418264042@qq.com>
2019-05-21 08:59:10 +0800

    修改ui.jpg

--- fronted/bookstore/src/App.vue
+++ fronted/bookstore/src/App.vue
@@ -35,13 +35,10 @@ var that=this;
   //监听事件，若有人收藏或者有人取消收藏，则会触发该事件
    
   io.socket.on('message', function (data) {
-   console.log('图书ID：');
-   console.log(data.id);
-   console.log("当今收藏人数：")
-    console.log(data.greeting);
+   
     that.$store.dispatch('getNewBookid',data.id); //保存图书id
     that.$store.dispatch('getNewNum',data.greeting); //保存图书收藏人数
-    console.log('socketbookid'+that.$store.state.bookid);
+    
   });
  
    
@@ -53,39 +50,7 @@ var that=this;
   
   methods:{
    
-  //   initWebSocket(){
-  //     if(typeof(WebSocket) === "undefined"){
-  //               alert("您的浏览器不支持socket")
-  //           }else{
-  //               // 实例化socket
-  //               this.socket = new WebSocket('ws://localhost:1337')
-  //               // 监听socket连接
-  //               this.socket.onopen = this.open
-  //               // 监听socket错误信息
-  //               this.socket.onerror = this.error
-  //               // 监听socket消息
-  //               this.socket.onmessage = this.getMessage
-  //           }
-  //   },
-  //   open(){
-  //     console.log("socket连接成功")
-  //     console.log(this.socket);
-  //   },
-  //   error(){
-  //     this.initWebSocket();
-  //     console.log("socket连接失败")
-  //   },
-  //   getMessage:function(e){
-  //      console.log(e);
-  //   },
-  //   send(){
-  //     let that=this;
-  //     this.socket.send(that.param);
-  //     console.log('发送成功');
-  //   },
-  //   close(){
-  //     console.log('断开连接');
-  //   },
+  
     
    },
   
